e1f6ed43c5e8b76587478a5cc8b11b8ef9a421dc
Yasmeen Wafa <yasmeenwafa@Yasmeens-MacBook-Pro.local>
2021-04-09 23:16:46 +0200

    new footer

--- footer.php
+++ footer.php
@@ -15,15 +15,14 @@ $lang = ICL_LANGUAGE_CODE;
 
 <footer class="footer-container">
 	<div class="footer-grid">
-		<div class="footer-start">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img class="logo" src="<?php echo get_stylesheet_directory_uri();?>/src/assets/images/footer-logo.png" alt=""></a>
-		</div>
-		<div class="footer-end">
-			<div class="footer-end-upper">
-				<div class="slogan">
-					<h2><?php _e("Start Your Dream", "homepage")?></h2>
-					<a class="cta-button" href="#"><span><?php _e('Contact Us', 'homepage')?></span></a>
-				</div>
+		<div class="footer-upper">		
+			<div class="footer-start-upper">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<img class="logo" src="<?php echo get_stylesheet_directory_uri();?>/src/assets/images/header-logo.png" alt="">
+				</a>
+			</div>
+			<div class="footer-center-upper">
+					
 				<?php 
 						wp_nav_menu( array( 
 						'theme_location' => 'footer-menu' ,
@@ -33,54 +32,35 @@ $lang = ICL_LANGUAGE_CODE;
 					
 						dynamic_sidebar( 'footer-widgets' );
 				?>
-
-				<!-- <div class="contact-info">
-					<p class="contact-title"><?php _e('Contacts',"homepage")?></p>
-					<p>
-						<?php if($lang == 'ar'):?>
-							<a href="<?php the_field("address_maps_link", 110)?>"><?php the_field('address', 110)?></a>
-							<?php else: ?>
-						<a href="<?php the_field("address_maps_link", 10)?>"><?php the_field('address', 10)?></a>
-						<?php endif;?>
-					</p>
-					<p>
-					<?php if($lang == 'ar'):?>
-						<a href="mailto:<?php the_field('email_address', 110)?>"><?php the_field('email_address', 110)?></a>
-							<?php else: ?>
-						<a href="mailto:<?php the_field('email_address', 10)?>"><?php the_field('email_address', 10)?></a>
-						<?php endif;?>
-					</p>
-					<p>
-					<?php if($lang == 'ar'):?>
-						<a href="tel:<?php the_field('phone_number', 110)?>"><?php the_field('phone_number', 110)?></a>
-
-							<?php else: ?>
-						<a href="tel:<?php the_field('phone_number', 10)?>"><?php the_field('phone_number', 10)?></a>
-						<?php endif;?>
-					</p>
-				</div> -->
-				<div class="social-media">
-					<a href="<?php the_field('facebook_link', 10)?>" target="_blank"><i class="icon-facebook"></i></a>
-					<a href="<?php the_field('instagram_link', 10)?>" target="_blank"><i class="icon-instagram"></i></a>
-					<a href="<?php the_field('youtube_link', 10)?>" target="_blank"><i class="icon-youtube-play"></i></a>
+			</div>
+			<div class="footer-end-upper">
+				<div class="slogan">
+					<h2><?php _e("Start Your Dream", "homepage")?></h2>
+					<a class="cta-button" href="#"><span><?php _e('Contact Us', 'homepage')?></span></a>
 				</div>
 			</div>
-			<div class="footer-end-lower">
-				<div class="partners">
-					<a href="" target="_blank"><img src="<?php echo get_stylesheet_directory_uri();?>/src/assets/images/partner1.png" alt=""></a>
-					<a href="" target="_blank"><img src="<?php echo get_stylesheet_directory_uri();?>/src/assets/images/partner2.png" alt=""></a>
+		</div>
+		<div class="footer-lower">
+				<div class="footer-start-lower">
+					<div class="social-media">
+						<a href="<?php the_field('facebook_link', 10)?>" target="_blank"><i class="icon-facebook"></i></a>
+						<a href="<?php the_field('instagram_link', 10)?>" target="_blank"><i class="icon-instagram"></i></a>
+						<a href="<?php the_field('youtube_link', 10)?>" target="_blank"><i class="icon-youtube-play"></i></a>
+					</div>
 				</div>
-				<div class="social-media">
-					<a href="<?php the_field('facebook_link', 10)?>" target="_blank"><i class="icon-facebook"></i></a>
-					<a href="<?php the_field('instagram_link', 10)?>" target="_blank"><i class="icon-instagram"></i></a>
-					<a href="<?php the_field('youtube_link', 10)?>" target="_blank"><i class="icon-youtube-play"></i></a>
-				</div>
-				<?php if($lang == "ar"):?>
+				<div class="footer-center-lower">
+					<?php if($lang == "ar"):?>
 						<p class="copyright">بلو <?php echo date("Y"); ?>. جميع الحقوق محفوظة</p>
 					<?php else:?>
 						<p class="copyright">Blue <?php echo date("Y"); ?>. All rights reserved</p>
 					<?php endif;?>
-			</div>
+				</div>
+				<div class="footer-end-lower">
+					<div class="partners">
+						<a href="" target="_blank"><img src="<?php echo get_stylesheet_directory_uri();?>/src/assets/images/partner1.png" alt=""></a>
+						<a href="" target="_blank"><img src="<?php echo get_stylesheet_directory_uri();?>/src/assets/images/partner2.png" alt=""></a>
+					</div>		
+				</div>
 		</div>
 	</div>
 </footer>
