fea935d15bd2373c08dac328bdedf0bf2f910b71
Yasmeen Wafa <yasmeenwafa@Yasmeens-MacBook-Pro.local>
2021-04-23 05:14:37 +0200

    services page done

--- page-templates/services.php
+++ page-templates/services.php
@@ -4,7 +4,17 @@ Template Name: Services
 */
 get_header(); ?>
 
-
-<h1>Services</h1>
+<div class="services-page-container">
+    <h1 class="title page-title"><span><?php _e('After Sale Services','services');?></span></h1>
+    <div class="services-image section-image">
+        <div class="overlay"></div>
+        <img src="<?php the_post_thumbnail_url('large'); ?>" alt="">
+    </div>
+    <div class="services-text animatedText">
+        <?php the_content();?>
+    </div>
+    <a href="<?php echo esc_url( home_url( '/contact-us' ) ); ?>" class="cta-button animatedText">
+    <span><?php _e('Request Proposal','services');?></span></a>
+</div>
 
 <?php get_footer();
